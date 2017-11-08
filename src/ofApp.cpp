d25007a991c8a9c5b103c3bc80981f400cafd24f
renegade17 <renegade17@users.noreply.github.com>
2017-11-08 15:48:15 -0400

    Normal distribution

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,16 +5,20 @@ void ofApp::setup(){
 	
 	std::random_device rd;
 	std::mt19937 eng1(rd());
-	std::uniform_int_distribution<> unifrm(0, 1200);
-	std::uniform_int_distribution<>unifrm1(0, 970);
+	std::normal_distribution<float> norm(1000, 15);
+	std::normal_distribution<float>norm1(800, 15);
 	
+	float mean=1000.00f; //average
+	float sd = 15.00f; //standard deviation
+	
+		
 jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
 /*X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
 Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel*/
 for (int n=0; n<20; n++)
 {
-	X.push_back (unifrm(eng1));
-	Y.push_back (unifrm1(eng1));
+	X.push_back (norm(eng1));
+	Y.push_back (norm1(eng1));
 }	
 }
 
